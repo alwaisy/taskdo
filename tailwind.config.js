c48b07638afe6906ef4aa5af11ae97654d4ddd29
Awais al-waisy <102944304+awaisalwaisy@users.noreply.github.com>
2023-04-08 00:43:25 +0500

    fonts are added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      inter: ['Inter', 'sans-serif']
+    }
   },
   plugins: [require('daisyui')]
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Unfonts from 'unplugin-fonts/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Unfonts({
+      /* options */
+      google: {
+        families: ['Inter']
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
