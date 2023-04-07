eac814fe7bfe86c1e5ea7e2ec32c1f755cc8d139
Awais al-waisy <102944304+awaisalwaisy@users.noreply.github.com>
2023-04-07 23:42:27 +0500

    initial commit -> daisy ui, heroicons are installed

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { BeakerIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+  <!-- <RouterView /> -->
+  <div>
+    <h1>App::TaskDo</h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <BeakerIcon class="h-6 w-6 text-blue-500" />
+    <button class="btn btn-success">Success</button>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped lang="postcss"></style>
